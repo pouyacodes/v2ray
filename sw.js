c6897126a4aa0277398fe99ab728511459495e81
Pouya <89411180+pouyacodes@users.noreply.github.com>
2025-12-30 18:40:15 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,17 +4,17 @@ const CACHE_NAME = `ray-${VERSION}`;
 
 const APP_STATIC_RESOURCES = [
 	'/',
-	'/index.html',
-	'/style.css',
-	'/script.js',
-	'/manifest.json',
-	'/sw.js',
-	'/assets/favicon.ico',
-	'/assets/Vazirmatn-Bold.woff2',
-	'/assets/Vazirmatn-Regular.woff2',
-	'/assets/icon-180.png',
-	'/assets/icon-192.png',
-	'/assets/icon-512.png',
+	'index.html',
+	'style.css',
+	'script.js',
+	'manifest.json',
+	'sw.js',
+	'assets/favicon.ico',
+	'assets/Vazirmatn-Bold.woff2',
+	'assets/Vazirmatn-Regular.woff2',
+	'assets/icon-180.png',
+	'assets/icon-192.png',
+	'assets/icon-512.png',
 ];
 
 self.addEventListener('install', (event) => {
